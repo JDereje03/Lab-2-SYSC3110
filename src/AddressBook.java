0e12a56d5a50b8affa9d63ad3019fd5f2b596b1b
josephdereje <josephdereje@cmail.carleton.ca>
2025-09-19 16:00:49 -0400

    test add/remove

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -19,7 +19,10 @@ public class AddressBook {
     }
 
     public static void main(String[] args){
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Homer", "123 Carleton street", "1234567890");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy("Homer");
     }
 
 }
